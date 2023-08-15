ea3cfe050d1eb78bb7562e8f9d2b9db80ffc0ffb
AnastasiaCarlii <124528864+AnastasiaCarlii@users.noreply.github.com>
2023-08-15 21:28:59 +0100

    Initial commit

---
+++ src/components/SearchFilter/Filter.jsx
@@ -0,0 +1,11 @@
+import PropTypes from 'prop-types';
+import { FilterInput } from './Filter.styled';
+export const Filter = ({ value, onChange }) => (
+  <label>
+    <FilterInput type="text" value={value} onChange={onChange} />
+  </label>
+);
+Filter.propTypes = {
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
---
+++ src/components/SearchFilter/Filter.styled.js
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+export const FilterInput = styled.input`
+  text-align: center;
+  color: black;
+  margin-bottom: 15px;
+  margin-left: 130px;
+`;
+// export const Filter = styled.div`
+//   display: flex;
+//   justify-content: center;
+//   text-align: center;
+// `;
